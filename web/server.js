ca55e2c4ee45e750be4c50873e6874aafc6dd40d
KwangJin <chgj1995@naver.com>
2024-06-07 23:48:58 +0900

    change web port
    port 수정

--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = 80;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
