d35af211c7894ec7fb943066c833e2dab578341f
KwangJin <46660737+chgj1995@users.noreply.github.com>
2024-07-10 10:55:55 +0900

    temporary remove nginx
    nginx없이도 접속자 ip 추적되는지 확인하기

--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const port = 3000;
+const port = 80;
 
 const API_BASE_URL = 'http://api:8090';
 
