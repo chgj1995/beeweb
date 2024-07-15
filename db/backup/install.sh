ce7b13aa686d941b9fdd1d08911958a261c2a110
KwangJin <46660737+chgj1995@users.noreply.github.com>
2024-07-15 19:42:59 +0900

    try to bugfix

--- db/backup/install.sh
+++ db/backup/install.sh
@@ -6,16 +6,13 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 # 모든 스크립트에 실행 권한 추가
 chmod +x $SCRIPT_DIR/*.sh
 
-# 크론 작업 명령어
-CRON_JOB="0 2 * * * $SCRIPT_DIR/backup.sh"
-
 # 현재 크론 작업 목록 가져오기
 CRON_JOBS=$(sudo crontab -l 2>/dev/null)
 
 # 크론 작업이 이미 존재하는지 확인하고, 없으면 추가
-if echo "$CRON_JOBS" | grep -q "$CRON_JOB"; then
+if echo "$CRON_JOBS" | grep -q "$SCRIPT_DIR/backup.sh"; then
   echo "Cron job already exists."
 else
-  (sudo crontab -l 2>/dev/null; echo "$CRON_JOB") | sudo crontab -
+  (sudo crontab -l 2>/dev/null; echo "0 2 * * * $SCRIPT_DIR/backup.sh") | sudo crontab -
   echo "Cron job installed to run backup.sh daily at 2 AM."
 fi
--- db/backup/uninstall.sh
+++ db/backup/uninstall.sh
@@ -3,10 +3,7 @@
 # 스크립트의 실제 경로 추적
 SCRIPT_DIR=$(dirname $(realpath $0))
 
-# 크론 작업 명령어
-CRON_JOB="0 2 * * * $SCRIPT_DIR/backup.sh"
-
 # 현재 크론 작업 목록에서 해당 작업 제거
-(sudo crontab -l 2>/dev/null | grep -v "$CRON_JOB") | sudo crontab -
+(sudo crontab -l 2>/dev/null | grep -v "$SCRIPT_DIR/backup.sh") | sudo crontab -
 
 echo "Cron job for backup.sh removed."
