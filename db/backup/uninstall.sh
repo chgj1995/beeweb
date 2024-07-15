4f173a1c8e11cb89f02549a65ac02561fd1fc2a8
KwangJin <46660737+chgj1995@users.noreply.github.com>
2024-07-15 19:39:38 +0900

    bugfix
    관리자 계정으로 동작하도록 수정

--- db/backup/uninstall.sh
+++ db/backup/uninstall.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 CRON_JOB="0 2 * * * $SCRIPT_DIR/backup.sh"
 
 # 현재 크론 작업 목록에서 해당 작업 제거
-(crontab -l 2>/dev/null | grep -v "$CRON_JOB") | crontab -
+(sudo crontab -l 2>/dev/null | grep -v "$CRON_JOB") | sudo crontab -
 
 echo "Cron job for backup.sh removed."
