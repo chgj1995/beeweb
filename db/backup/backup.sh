4b0a3b768ada93d658ee601f1590bc73cfd2e5dd
KwangJin <46660737+chgj1995@users.noreply.github.com>
2024-07-19 11:01:15 +0900

    bugfix
    설치스크립트랑 backup스크립트에 절대경로 사용

--- db/backup/backup.sh
+++ db/backup/backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -x
+
+# Docker 명령어 절대 경로
+DOCKER_PATH=/snap/bin/docker
+
 # 스크립트의 실제 경로 추적
 SCRIPT_DIR=$(dirname $(realpath $0))
 
@@ -17,7 +22,7 @@ ERROR_LOG="$HOST_BACKUP_DIR/mysqldump_error.log"
 mkdir -p $HOST_BACKUP_DIR
 
 # Docker 컨테이너 ID 가져오기
-CONTAINER_ID=$(docker ps -q -f "name=beeweb-mariadb")
+CONTAINER_ID=$($DOCKER_PATH ps -q -f "name=beeweb-mariadb")
 
 # 디버깅 정보 출력
 echo "SCRIPT_DIR: $SCRIPT_DIR"
@@ -32,19 +37,19 @@ if [ -z "$CONTAINER_ID" ]; then
   exit 1
 fi
 
-# Docker 컨테이너 내부에 백업 디렉토리 생성
-docker exec $CONTAINER_ID mkdir -p $CONTAINER_BACKUP_DIR
+# Docker 컨테이너 내부에 백업 디렉토리 생
+$DOCKER_PATH exec $CONTAINER_ID mkdir -p $CONTAINER_BACKUP_DIR
 
 # Docker 컨테이너에서 백업 명령 실행 및 오류 로그 캡처
-docker exec $CONTAINER_ID sh -c "mysqldump --column-statistics=0 -u root -prootpassword hive_data > $BACKUP_FILE" 2> $ERROR_LOG
+$DOCKER_PATH exec $CONTAINER_ID sh -c "mysqldump --column-statistics=0 -u root -prootpassword hive_data > $BACKUP_FILE" 2> $ERROR_LOG
 EXEC_STATUS=$?
 
 # 백업이 성공했는지 확인
 if [ $EXEC_STATUS -eq 0 ]; then
   # 백업 파일을 호스트로 복사
-  docker cp $CONTAINER_ID:$BACKUP_FILE $HOST_BACKUP_FILE
+  $DOCKER_PATH cp $CONTAINER_ID:$BACKUP_FILE $HOST_BACKUP_FILE
   COPY_STATUS=$?
-  
+
   if [ $COPY_STATUS -eq 0 ]; then
     echo "Backup successful: $HOST_BACKUP_FILE"
 
@@ -57,3 +62,5 @@ else
   echo "Backup failed"
   echo "Check the log file for more details: $ERROR_LOG"
 fi
+
+set +x
--- db/backup/install.sh
+++ db/backup/install.sh
@@ -6,6 +6,12 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 # 모든 스크립트에 실행 권한 추가
 chmod +x $SCRIPT_DIR/*.sh
 
+# DOCKER_PATH 설정
+DOCKER_PATH=$(which docker)
+
+# backup.sh 파일에서 DOCKER_PATH를 설정
+sed -i "s|^DOCKER_PATH=.*|DOCKER_PATH=$DOCKER_PATH|" $SCRIPT_DIR/backup.sh
+
 # 현재 크론 작업 목록 가져오기
 CRON_JOBS=$(sudo crontab -l 2>/dev/null)
 
@@ -13,6 +19,6 @@ CRON_JOBS=$(sudo crontab -l 2>/dev/null)
 if echo "$CRON_JOBS" | grep -q "$SCRIPT_DIR/backup.sh"; then
   echo "Cron job already exists."
 else
-  (sudo crontab -l 2>/dev/null; echo "0 2 * * * $SCRIPT_DIR/backup.sh") | sudo crontab -
+  (sudo crontab -l 2>/dev/null; echo "* * * * * $SCRIPT_DIR/backup.sh >> /home/wcl/workspace/beeweb/db/backup/data/backup_cron.log 2>&1") | sudo crontab -
   echo "Cron job installed to run backup.sh daily at 2 AM."
 fi
